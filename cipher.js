5de7720149977822cb560c40bed90a4cf9e54194
LexSava <lexsava@gmail.com>
2021-05-09 15:33:05 +0300

    feat: added function decode
    added function decode

--- cipher.js
+++ cipher.js
@@ -1,30 +1,75 @@
-const caesarCipher = (str, shift) => {
+const caesarCipher = (str, shift, action) => {
 
   const alphabetLength = 26;
   const upperCharCode = 65;
   const lowerCharCode = 97;
 
-  const shiftUpperChar = (char) => {
-    let newCharCode = (char.charCodeAt(0) - upperCharCode + shift) % alphabetLength;
-    if (newCharCode < 0) {
-      newCharCode += alphabetLength;
-    }
-    newCharCode += upperCharCode;
-    return String.fromCharCode(newCharCode);
-  };
+  if (action === 'encode') {
+    const shiftUpperChar = (char) => {
+      let newCharCode = (char.charCodeAt(0) - upperCharCode + shift) % alphabetLength;
+      if (newCharCode < 0) {
+        newCharCode += alphabetLength;
+      }
+      newCharCode += upperCharCode;
+      return String.fromCharCode(newCharCode);
+    };
 
-  const shiftLowerChar = (char) => {
-    let newCharCode = (char.charCodeAt(0) - lowerCharCode + shift) % alphabetLength;
-    if (newCharCode < 0) {
-      newCharCode += alphabetLength;
-    }
-    newCharCode += lowerCharCode;
-    return String.fromCharCode(newCharCode);
-  };
+    const shiftLowerChar = (char) => {
+      let newCharCode = (char.charCodeAt(0) - lowerCharCode + shift) % alphabetLength;
+      if (newCharCode < 0) {
+        newCharCode += alphabetLength;
+      }
+      newCharCode += lowerCharCode;
+      return String.fromCharCode(newCharCode);
+    };
+    return str
+      .replace(/[A-Z]/g, shiftUpperChar)
+      .replace(/[a-z]/g, shiftLowerChar);
 
-  return str
-    .replace(/[A-Z]/g, shiftUpperChar)
-    .replace(/[a-z]/g, shiftLowerChar);
+  } else if (action === 'decode') {
+    const shiftUpperChar = (char) => {
+      let newCharCode = (char.charCodeAt(0) - upperCharCode - shift) % alphabetLength;
+      if (newCharCode < 0) {
+        newCharCode += alphabetLength;
+      }
+      newCharCode += upperCharCode;
+      return String.fromCharCode(newCharCode);
+    };
+
+    const shiftLowerChar = (char) => {
+      let newCharCode = (char.charCodeAt(0) - lowerCharCode - shift) % alphabetLength;
+      if (newCharCode < 0) {
+        newCharCode += alphabetLength;
+      }
+      newCharCode += lowerCharCode;
+      return String.fromCharCode(newCharCode);
+    };
+    return str
+      .replace(/[A-Z]/g, shiftUpperChar)
+      .replace(/[a-z]/g, shiftLowerChar);
+  }
+
+  // const shiftUpperChar = (char) => {
+  //   let newCharCode = (char.charCodeAt(0) - upperCharCode + shift) % alphabetLength;
+  //   if (newCharCode < 0) {
+  //     newCharCode += alphabetLength;
+  //   }
+  //   newCharCode += upperCharCode;
+  //   return String.fromCharCode(newCharCode);
+  // };
+
+  // const shiftLowerChar = (char) => {
+  //   let newCharCode = (char.charCodeAt(0) - lowerCharCode + shift) % alphabetLength;
+  //   if (newCharCode < 0) {
+  //     newCharCode += alphabetLength;
+  //   }
+  //   newCharCode += lowerCharCode;
+  //   return String.fromCharCode(newCharCode);
+  // };
+
+  // return str
+  //   .replace(/[A-Z]/g, shiftUpperChar)
+  //   .replace(/[a-z]/g, shiftLowerChar);
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -15,20 +15,10 @@ const readline = require('readline');
 
 const argv = require('minimist')(process.argv.slice(2));
 
-// console.log(argv);
-
-// $ node my_caesar_cli -a encode -s 7 -i "./input.txt" -o "./output.txt"
-// $ node my_caesar_cli --action encode --shift 7 --input plain.txt --output encoded.txt
-
-// const args = parseArgs(process.argv.slice(2));
-
-console.log(argv);
-
 let action;
 let shift;
 let input;
 let output;
-
 
 Object.keys(argv).forEach((argItem) => {
   switch (argItem) {
@@ -68,46 +58,42 @@ const CheckValidityFiles = (() => {
   }
 })();
 
-if (action === "encode" || action === "encode") {
+
   if (input === 'input.txt' && output === 'output.txt') {
-    console.log('introduced input and output');
+    console.log(chalk.bgGreen.black('introduced input and output'));
     fs.readFile(inputFile, 'utf-8', (err, content) => {
       if (err) {
         throw err;
       }
-      fs.appendFile(outputFile, caesarCipher(`${content}\n`, shift), err => {
+      fs.appendFile(outputFile, caesarCipher(`${content}\n`, shift, action), err => {
         if (err) {
           throw err;
         }
       })
     })
   }
-}
 
-if (action === "encode" || action === "encode") {
   if (!argv.i && !argv.input && !argv.o && !argv.output) {
-    console.log('no input and output');
+    console.log(chalk.bgGreen.black('no input and output'));
     const eventUser = readline.createInterface({ input: process.stdin, output: process.stdout });
     eventUser.question(chalk.blue(`Enter the data: \n`), (userInput) => {
       if (userInput.length > 0) {
-        console.log(caesarCipher(`${userInput}\n`, shift));
+        console.log(caesarCipher(`${userInput}\n`, shift, action));
       }
       eventUser.on('line', (userInput) => {
         if (userInput.length > 0) {
-          console.log(caesarCipher(`${userInput}\n`, shift));
+          console.log(caesarCipher(`${userInput}\n`, shift, action));
         }
       })
     })
   }
-}
 
-if (action === "encode" || action === "encode") {
   if (input === undefined && output === 'output.txt') {
-    console.log('no input but there is output');
+    console.log(chalk.bgGreen.black('no input but there is output'));
     const eventUser = readline.createInterface({ input: process.stdin, output: process.stdout });
     eventUser.question(chalk.blue(`Enter the data: \n`), (userInput) => {
       if (userInput.length > 0) {
-        fs.appendFile(outputFile, caesarCipher(`${userInput}\n`, shift), err => {
+        fs.appendFile(outputFile, caesarCipher(`${userInput}\n`, shift, action), err => {
           if (err) {
             throw err;
           }
@@ -115,7 +101,7 @@ if (action === "encode" || action === "encode") {
       }
       eventUser.on('line', (userInput) => {
         if (userInput.length > 0) {
-          fs.appendFile(outputFile, caesarCipher(`${userInput}\n`, shift), err => {
+          fs.appendFile(outputFile, caesarCipher(`${userInput}\n`, shift, action), err => {
             if (err) {
               throw err;
             }
@@ -124,17 +110,15 @@ if (action === "encode" || action === "encode") {
       })
     })
   }
-}
 
-if (action === "encode" || action === "encode") {
   if (output === undefined && input === 'input.txt') {
-    console.log('no output but there is input');
+    console.log(chalk.bgGreen.black('no output but there is input'));
     fs.readFile(inputFile, 'utf-8', (err, content) => {
       if (err) {
         throw err;
       }
-      console.log(caesarCipher(`${content}\n`, shift));
+      console.log(caesarCipher(`${content}\n`, shift, action));
     })
   }
-}
+
 
